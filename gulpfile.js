36ab7d00dbf9f3496c17cc85c34ac4eb0a2d1928
qimengjie <qimengjie@163.com>
2019-07-29 20:29:05 +0800

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,28 @@
 const gulp = require("gulp")
-const browserify = require("browserify")
-const source = require('vinyl-source-stream')
-const watchify = require("watchify")
-const tsify = require("tsify")
+const ts = require('gulp-typescript')
+const tsProject = ts.createProject('tsconfig.json')
+const concat = require('gulp-concat')
 const sourcemaps = require('gulp-sourcemaps')
-const buffer = require('vinyl-buffer')
-const fancy_log = require("fancy-log")
-const paths = {
-  pages: ['src/*.html']
-}
-
-const watchedBrowserify = watchify(browserify({
-  basedir: '.',
-  debug: true,
-  entries: ['src/main.ts'],
-  cache: {},
-  packageCache: {}
-}).plugin(tsify))
-
-gulp.task("copy-html", function () {
-  return gulp.src(paths.pages)
+ 
+gulp.task("copy-html", function() {
+  return gulp.src('src/**/*.html')
     .pipe(gulp.dest("dist"))
 })
 
-function bundle() {
-  console.log('update')
-  return watchedBrowserify
-      .transform('babelify', {
-        presets: ['es2015'],
-        extensions: ['.ts']
-      })
-      .bundle()
-      .pipe(source('bundle.js'))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({loadMaps: true}))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('dist'))
-}
+gulp.task("build-ts", function() {
+  return gulp.src('src/**/*.ts')
+    .pipe(tsProject())
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'))
+})
 
-gulp.task("default", gulp.series(gulp.parallel('copy-html'), bundle))
+gulp.task("watch", function(done) {
+  gulp.watch('src/**/*.ts', gulp.series('build-ts'))
+  gulp.watch('src/**/*.html', gulp.series('copy-html'))
+  done()
+})
 
-watchedBrowserify.on("update", bundle)
-watchedBrowserify.on("log", fancy_log)
+gulp.task("default", gulp.series('copy-html', 'build-ts'))
+gulp.task('dev', gulp.series('copy-html', 'build-ts', 'watch'))
