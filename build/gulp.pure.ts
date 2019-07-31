333bb3d8b2bacb8cd8b3ea1c1dd090b89bf511d7
qimengjie <qimengjie@163.com>
2019-07-31 20:24:19 +0800

    Update

--- build/gulp.pure.ts
+++ build/gulp.pure.ts
@@ -1,76 +1,109 @@
 import * as path from "path"
 import * as fs from "fs-extra"
 import * as gulp from "gulp"
-import * as concat from "gulp-concat"
 import * as sourcemaps from "gulp-sourcemaps"
 import * as ts from "gulp-typescript"
 import * as gulpIf from "gulp-if"
 import * as log from "fancy-log"
+import * as vinyl from "vinyl"
 import chalk from "chalk"
 
 const tsProject = ts.createProject('tsconfig.json')
 
-const isMuti = true
-
 const PATHS = {
   dist: path.resolve(__dirname, '../dist'),
-  src: path.resolve(__dirname, '../src'),
-  page: [`${path.resolve(__dirname, '../src')}/**/*.html`],
-  ts: [`${path.resolve(__dirname, '../src')}/**/*.ts`]
+  src: path.resolve(__dirname, '../src/**')
 }
+
+type Done = (error?: any) => void
 
 export class Pure {
   constructor() {
     this.init()
   }
   init() {
-    gulp.task("cleanDist", function(done: any) {
+    gulp.task("cleanDist", function(done: Done) {
       fs.emptyDirSync(PATHS.dist)
       done()
     })
-    
-    gulp.task("copy-html", function() {
-      return gulp.src(PATHS.page)
-        .pipe(gulp.dest(PATHS.dist))
-    })
-    
-    function buildTs (path?: string) {
-      log.info(`Starting ${chalk.cyan("'buildTs'")} ${chalk.magenta(path || '')}`)
-      return gulp.src(path || PATHS.ts)
-        .pipe(tsProject())
-        .pipe(sourcemaps.init())
-        .pipe(gulpIf(!isMuti, concat('bundle.js')))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(PATHS.dist))
+
+    function getSuffix(filename: string) {
+      const index = filename.lastIndexOf(".")
+      return filename.substr(index + 1)
+    }
+
+    function src2dist(filename: string) {
+      const relativePath = path.relative(__dirname, filename).split(path.sep).join('/')
+      const projectPath = relativePath.replace(/^(..\/)*src\//g, '')
+      const distPath = path.join(PATHS.dist, projectPath)
+      return distPath
+    }
+
+    function isTs(fs: vinyl) {
+      return fs.extname === '.ts'
+    }
+
+    function buildFile (filename?: string) {
+      log.info(`Starting ${chalk.cyan("'buildFile'")} ${chalk.magenta(filename || '')}`)
+
+      let basePath: string,
+          srcPath: string | string[],
+          stream: NodeJS.ReadableStream
+      if (filename) {
+        basePath = path.resolve(__dirname, '../src')
+        srcPath = filename
+
+        stream = gulp.src(srcPath, { base: basePath })
+        if (getSuffix(filename) === 'ts') {
+          stream = stream
+            .pipe(tsProject())
+            .pipe(sourcemaps.init())
+            .pipe(sourcemaps.write())
+        }
+        stream = stream
+          .pipe(gulp.dest(PATHS.dist))
+      } else {
+        basePath = ''
+        srcPath = PATHS.src
+
+        stream = gulp
+          .src(srcPath, { base: basePath })
+          .pipe(
+            gulpIf(isTs, tsProject())
+          )
+          .pipe(sourcemaps.init())
+          .pipe(sourcemaps.write())
+          .pipe(gulp.dest(PATHS.dist))
+      }
+      return stream
         .on('end', () => {
-          log.info(`Finished ${chalk.cyan("'buildTs'")}`)
+          log.info(`Finished ${chalk.cyan("'buildFile'")}`)
         })
     }
     
-    gulp.task("build-ts", function() {
-      return buildTs()
+    gulp.task("build-file", function() {
+      return buildFile()
     })
-    
-    gulp.task("watch", function(done: any) {
-      gulp.watch(PATHS.page, gulp.series('copy-html'))
-      const tsWatcher = gulp.watch(PATHS.ts)
-      ;['change', 'add'].forEach(t => {
-        tsWatcher.on(t, (_path: string): void => {
-          buildTs(_path)
+
+    gulp.task("watch", function(done: Done) {
+      const tsWatcher = gulp.watch(PATHS.src)
+      ;['change', 'add'].forEach(event => {
+        tsWatcher.on(event, (filename: string) => {
+          buildFile(filename)
         })
       })
-      ;['unlink'].forEach(t => {
-        tsWatcher.on(t, (_path: string): void => {
-          const relativePath = path.relative(__dirname, _path).split(path.sep).join('/')
-          const projectPath = relativePath.replace(/^(..\/)*src\//g, '')
-          const distPath = path.join(PATHS.dist, projectPath).replace('.ts', '.js')
-          fs.removeSync(distPath)
+      ;['unlink', 'unlinkDir'].forEach(event => {
+        tsWatcher.on(event, (filename: string) => {
+          log.info(`Starting ${chalk.cyan("'delete'")} ${chalk.magenta(filename || '')}`)
+          fs.remove(src2dist(filename).replace('.ts', '.js'), function() {
+            log.info(`Finished ${chalk.cyan("'delete'")}`)
+          })
         })
       })
       done()
     })
     
-    gulp.task("default", gulp.series('cleanDist', 'copy-html', 'build-ts'))
-    gulp.task('dev', gulp.series('cleanDist', 'copy-html', 'build-ts', 'watch'))
+    gulp.task("default", gulp.series('cleanDist', 'build-file'))
+    gulp.task('dev', gulp.series('cleanDist', 'build-file', 'watch'))
   }
 }
