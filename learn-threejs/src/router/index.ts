a83cb266f68c40ab1bb8c836e65c590982f24457
qimengjie <qimengjie@163.com>
2020-01-10 08:46:06 +0800

    feat: css3renderer

--- learn-threejs/src/router/index.ts
+++ learn-threejs/src/router/index.ts
@@ -1,8 +1,10 @@
 import hello from '../pages/1.hello'
 import camera from '../pages/2.camera'
 import geometry from '../pages/3.geometry'
+import css3dRenderer from '../pages/4.css3drenderer'
 export default {
   hello,
   camera,
-  geometry
+  geometry,
+  css3dRenderer
 }
